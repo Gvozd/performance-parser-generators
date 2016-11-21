54c0280b5de2cf9e6adeb39e8298ccb34e3d56fa
Gvozd <gvozdev.viktor@gmail.com>
2016-11-22 00:29:54 +0300

    jison-plus/crashed

---
+++ test/jison-plus/index.js
@@ -0,0 +1,12 @@
+// var fs = require('fs');
+// var path = require('path');
+// var expect = require('chai').expect;
+// var Parser = require("jison-plus").Parser;
+//
+// module.exports.calc = new Parser(fs.readFileSync(path.join(__dirname, './calc.jison'), {encoding: 'utf8'}));
+//
+// describe('jison', function() {
+//     it('simple', function() {
+//         expect(module.exports.calc.parse('2 + 2 * 2')).to.equal(6);
+//     });
+// });
