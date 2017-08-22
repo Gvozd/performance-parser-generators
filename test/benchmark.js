fb0fe1901106af80351d2dff335d15609318998c
Gvozd <gvozdev.viktor@gmail.com>
2017-08-22 09:40:45 +0300

    add TODO's

--- test/benchmark.js
+++ test/benchmark.js
@@ -9,6 +9,11 @@ require('./kison/index');
 require('./packrattle/index');
 
 describe('benchmark', function() {
+    // TODO SimplePEG https://www.npmjs.com/package/simplepeg
+    // TODO https://www.npmjs.com/package/parsly
+    // TODO подсмотреть грамматику JS в https://habrahabr.ru/post/191858/
+    // TODO http://neerc.ifmo.ru/wiki/index.php?title=%D0%A3%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5_%D0%BB%D0%B5%D0%B2%D0%BE%D0%B9_%D1%80%D0%B5%D0%BA%D1%83%D1%80%D1%81%D0%B8%D0%B8
+    // https://habrahabr.ru/post/224081/
     var jisonParser = require('./jison/index').calc;
     var pegjsParser = require('./pegjs/index').calc;
     var pegjsFnParser = require('./pegjs-fn/index').calc;
